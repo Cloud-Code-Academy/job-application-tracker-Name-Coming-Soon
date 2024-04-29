9b5ba984003eef7e8374b091fb0fb079e1c4b45e
PoojaPareek363 <poojapareek1912@gmail.com>
2024-04-28 19:05:58 -0700

    Interviewhelper + test class + LWC calculator

--- force-app/main/default/lwc/takeHomePayCalculator/takeHomePayCalculator.js
+++ force-app/main/default/lwc/takeHomePayCalculator/takeHomePayCalculator.js
@@ -1,10 +1,11 @@
-import { LightningElement } from 'lwc';
+import { LightningElement} from 'lwc';
 
 const medicareRate = 0.0145;
 const socialSecurityRate = 0.0620;
-const federalStandardDeduction = 12950; // Federal standard deduction for single filer
-const stateStandardDeduction = 4803; // State standard deduction for single filer
+const federalStandardDeduction = 12950;
+const stateStandardDeduction = 4803;
 const totalStandardDeduction = federalStandardDeduction + stateStandardDeduction;
+
 const federalTaxBrackets = [
   { min: 0, max: 11000, rate: 0.10 },
   { min: 11001, max: 43750, rate: 0.12 },
@@ -28,21 +29,15 @@ export default class TakeHomePayCalculator extends LightningElement {
   biWeeklyPay = 0;
 
   handleIncomeChange(event) {
-    this.income = event.target.value;
-    this.Calculate();
+    this.income = parseFloat(event.target.value);
+    this.handleCalculate();
   }
 
-  preventDefaultAndHandleCalculate(event) {
-    console.log('preventDefaultAndHandleCalculate called');
-    event.preventDefault(); // Prevent the default button behavior (page refresh)
+  handleCalculateClick = (event) => {
     this.handleCalculate();
   }
 
   handleCalculate() {
-    this.Calculate();
-  }
-
-  Calculate() {
     this.federalWithholding = this.calculateFederalTax().toFixed(2);
     this.taxableIncome = this.income > totalStandardDeduction ? this.income - totalStandardDeduction : this.income;
     this.medicareWithholding = (this.income * medicareRate).toFixed(2);
@@ -52,20 +47,6 @@ export default class TakeHomePayCalculator extends LightningElement {
     this.semiAnnualPay = (this.takeHomePay / 2).toFixed(2);
     this.monthlyPay = (this.takeHomePay / 12).toFixed(2);
     this.biWeeklyPay = (this.takeHomePay / 26).toFixed(2);
-
-    this.updateInputFields();
-  }
-
-  updateInputFields() {
-    this.template.querySelector('#text-input-salarydeductions').value = this.salaryDeductions;
-    this.template.querySelector('#text-input-federaltax').value = this.federalWithholding;
-    this.template.querySelector('#text-input-medicare').value = this.medicareWithholding;
-    this.template.querySelector('#text-input-socialsecurity').value = this.socialSecurityWithholding;
-    this.template.querySelector('#text-input-taxableincome').value = this.taxableIncome;
-    this.template.querySelector('#text-input-takehomepay').value = this.takeHomePay;
-    this.template.querySelector('#text-input-semestralpay').value = this.semiAnnualPay;
-    this.template.querySelector('#text-input-monthlypay').value = this.monthlyPay;
-    this.template.querySelector('#text-input-biweeklypay').value = this.biWeeklyPay;
   }
 
   calculateFederalTax() {
